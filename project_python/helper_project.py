8b6f14e0c7bdacf598701c9575d447f10d502cc8
samsudin <samsudin@privyid.tech>
2024-09-19 20:19:52 +0700

    Add new file helper_project

--- project_python/helper_project.py
+++ project_python/helper_project.py
@@ -1,4 +1,23 @@
 from datetime import datetime
+import os
+import time 
+import time
+
+# basic function
+def basic_func(x):
+    if x == 0:
+        return 'zero'
+    elif x%2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+
+# worker sleep
+def worker():
+    time.sleep(1)
+    starttime = time.time()
+    print(f"thread Id : {threading.get_ident()},process ID: {os.getpid()},timestamp : {starttime}")
+
 
 # this function for convert datetime
 def datetime_conv(row_data):
--- project_python/test_another_multiprocess.py
+++ project_python/test_another_multiprocess.py
@@ -1,11 +1,3 @@
-import time
-
-def basic_func(x):
-    if x == 0:
-        return 'zero'
-    elif x%2 == 0:
-        return 'even'
-    else:
-        return 'odd'
+from helper_project import basic_func
     
 basic_func(10)
