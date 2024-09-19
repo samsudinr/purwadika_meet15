6bcc16ec7aa4fc70efceb07e8ef699489767aab3
samsudin <samsudin@privyid.tech>
2024-09-19 21:23:35 +0700

    project: revise file ge_name

--- project_python/get_nama.py
+++ project_python/get_nama.py
@@ -3,17 +3,3 @@ def getHello(nama):
 
 alpha = input("Masukan Nama")
 getHello(alpha)
-print('The code is done')
-
-# basic function
-def basic_func(x):
-    if x == 0:
-        return 'zero'
-    elif x%2 == 0:
-        return 'even'
-    else:
-        return 'odd'
-
-for i in alpha:
-    print(i)
-    print(len(alpha))
